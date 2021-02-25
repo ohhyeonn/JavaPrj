3cca23f2a0e06f23fa2e8fb309189040e1754612
ohhyeon <hyunong2@gmail.com>
2021-02-26 08:18:29 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+
+	public static void main(String[] args) {
+
+		int kor, eng;
+		int math;
+		int total;
+		double avg;
+
+		kor = 60;
+		eng = 70;
+		math = 80;
+
+		total = kor + eng + math;
+		avg = total / 3;
+
+		System.out.printf("total is %d \n", total);
+	}
+
+}
