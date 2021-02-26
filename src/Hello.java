378a5f1af0fcec5a1483433a3f29991fe8a9cbb7
ohhyeonn <hyunong2@gmail.com>
2021-02-26 12:25:30 +0900

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -7,14 +7,23 @@ public class Hello {
 		int total;
 		double avg;
 
-		kor = 60;
-		eng = 70;
-		math = 80;
+		kor = 65;
+		eng = 72;
+		math = 86;
 
 		total = kor + eng + math;
-		avg = total / 3;
+		avg = total / 3.0;
 
-		System.out.printf("total is %d \n", total);
+		System.out.println("┌─────────────────────────┐");
+		System.out.println("│        성적 출력		  │");
+		System.out.println("└─────────────────────────┘");
+		System.out.printf("국어 : %d\n",kor);
+		System.out.printf("영어 : %d\n",eng);
+		System.out.printf("수학 : %d\n",math);
+		System.out.printf("총점 : %d\n",total);
+		System.out.printf("평균 : %-6.2f\n",avg);
+		System.out.println("───────────────────────────");
+		
 	}
 
 }
