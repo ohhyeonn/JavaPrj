f8e9577bdad2aa3bc266dcf7e55ddde6087c9947
ohhyeonn <hyunong2@gmail.com>
2021-03-04 13:15:59 +0900

    bmp FileOutputStream

--- src/ex1/bit/Program.java
+++ src/ex1/bit/Program.java
@@ -2,12 +2,49 @@ package ex1.bit;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Program {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 
-		FileInputStream fis  = new FileInputStream("C:\\tool\\eclipse-workspace\\JavaPrj\\img.bmp");
+		FileInputStream fis  = new FileInputStream(".\\res\\img.bmp");
+		
+		int b;
+		b = fis.read();
+		b = fis.read();
+		
+		
+		int b1 = fis.read();
+		int b2 = fis.read();
+		int b3 = fis.read();
+		int b4 = fis.read();
+		
+		int fileSize = 0;
+		
+		
+		
+		
+		fileSize = ((b4&0x000000ff)<<24) |
+				   ((b3&0x000000ff)<<16) |
+				   ((b2&0x000000ff)<<8)|
+				   (b1);
+		
+		
+		
+		
+		System.out.println(Integer.toBinaryString(b4));
+		System.out.println(Integer.toBinaryString(b3));
+		System.out.println(Integer.toBinaryString(b2));
+		System.out.println(Integer.toBinaryString(b1));
 
-}
+		System.out.printf("%d", fileSize);
+		
+	
+		
+
+		
+		fis.close();
+		
+}		
 }
