b2b47235e8d17093d1f8215f4038ddecce0218bd
ohhyeonn <hyunong2@gmail.com>
2021-03-08 12:47:46 +0900

    while dowhile for homework

---
+++ src/ex2/control/ProgramMenuSwitch2.java
@@ -0,0 +1,103 @@
+package ex2.control;
+
+import java.util.Scanner;
+
+public class ProgramMenuSwitch2 {
+
+	public static void main(String[] args) {
+		int kor, eng;
+		int math;
+		int total;
+		double avg;
+
+		kor = 0;
+		eng = 0;
+		math = 0;
+
+		boolean more = true;
+		Scanner scan = new Scanner(System.in);
+
+		EXIT: while (true) {
+
+			int menu = 0;
+			System.out.println("┌─────────────────────────┐");
+			System.out.println("│        메인 메뉴		  │");
+			System.out.println("└─────────────────────────┘");
+			System.out.println("1.성적 입력 ");
+			System.out.println("2.성적 출력 ");
+			System.out.println("3.종료 ");
+			System.out.println("숫자를 입력하세요");
+			menu = Integer.parseInt(scan.nextLine());
+
+			switch (menu) {
+
+			case 1:
+
+				System.out.println("┌─────────────────────────┐");
+				System.out.println("│        성적 입력		  │");
+				System.out.println("└─────────────────────────┘");
+
+
+				do {
+
+					System.out.print("국어성적을 입력하세요 : ");
+					kor = Integer.parseInt(scan.nextLine());
+
+					if(!(kor <= 100 && kor >= 0))
+						System.out.println("0 ~ 100 범위의 값만 입력 받을수 있습니다");
+				}while(!(kor <= 100 && kor >= 0));	
+				
+				
+				
+				
+
+				System.out.print("영어성적을 입력하세요 : ");
+				eng = Integer.parseInt(scan.nextLine());
+
+				System.out.print("수학성적을 입력하세요 : ");
+				math = Integer.parseInt(scan.nextLine());
+
+				System.out.println("───────────────────────────");
+				break;
+
+			case 2:
+				total = kor + eng + math;
+				avg = total / 3.0;
+
+				System.out.println("┌─────────────────────────┐");
+				System.out.println("│        성적 출력		  │");
+				System.out.println("└─────────────────────────┘");
+				
+				
+				for (int i =0; i<3; i++) {
+				System.out.printf("국어 : %-6.2f\n", (double) kor);
+				System.out.printf("영어 : %-6.2f\n", (double) eng);
+				System.out.printf("수학 : %-6.2f\n", (double) math);
+				System.out.printf("총점 : %-6.2f\n", (double) total);
+				System.out.printf("평균 : %-6.2f\n", avg);
+				System.out.println("───────────────────────────");
+				}
+				
+				
+				
+				
+				
+				
+				System.out.println("계속할시 true, 그만할시 false");
+				more = Boolean.parseBoolean(scan.nextLine());
+				if (!more)
+					break EXIT;
+				
+				break;
+
+			case 3:
+
+				break EXIT;
+
+			}
+		}
+		System.out.println("good bye~");
+
+	}
+
+}
---
+++ src/homework/Homework_20210308.java
@@ -0,0 +1,101 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Homework_20210308 {
+
+	public static void main(String[] args) {
+		int kor, eng;
+		int math;
+		int total;
+		double avg;
+
+		kor = 0;
+		eng = 0;
+		math = 0;
+
+		boolean more = true;
+		Scanner scan = new Scanner(System.in);
+
+		EXIT: while (true) {
+
+			int menu = 0;
+			System.out.println("┌─────────────────────────┐");
+			System.out.println("│        메인 메뉴		  │");
+			System.out.println("└─────────────────────────┘");
+			System.out.println("1.성적 입력 ");
+			System.out.println("2.성적 출력 ");
+			System.out.println("3.종료 ");
+			System.out.println("숫자를 입력하세요");
+			menu = Integer.parseInt(scan.nextLine());
+
+			switch (menu) {
+
+			case 1:
+
+				System.out.println("┌─────────────────────────┐");
+				System.out.println("│        성적 입력		  │");
+				System.out.println("└─────────────────────────┘");
+
+				do {
+
+					System.out.print("국어성적을 입력하세요 : ");
+					kor = Integer.parseInt(scan.nextLine());
+
+					if (!(kor <= 100 && kor >= 0))
+						System.out.println("0 ~ 100 범위의 값만 입력 받을수 있습니다");
+				} while (!(kor <= 100 && kor >= 0));
+
+				do {
+
+					System.out.print("영어성적을 입력하세요 : ");
+					eng = Integer.parseInt(scan.nextLine());
+
+					if (!(eng <= 100 && eng >= 0))
+						System.out.println("0 ~ 100 범위의 값만 입력 받을수 있습니다");
+				} while (!(eng <= 100 && eng >= 0));
+
+				do {
+					System.out.print("수학성적을 입력하세요 : ");
+					math = Integer.parseInt(scan.nextLine());
+
+					if (!(math <= 100 && math >= 0))
+						System.out.println("0 ~ 100 범위의 값만 입력 받을수 있습니다");
+				} while (!(math <= 100 && math >= 0));
+
+				System.out.println("───────────────────────────");
+				break;
+
+			case 2:
+				total = kor + eng + math;
+				avg = total / 3.0;
+
+				System.out.println("┌─────────────────────────┐");
+				System.out.println("│        성적 출력		  │");
+				System.out.println("└─────────────────────────┘");
+
+				System.out.printf("국어 : %-6.2f\n", (double) kor);
+				System.out.printf("영어 : %-6.2f\n", (double) eng);
+				System.out.printf("수학 : %-6.2f\n", (double) math);
+				System.out.printf("총점 : %-6.2f\n", (double) total);
+				System.out.printf("평균 : %-6.2f\n", avg);
+				System.out.println("───────────────────────────");
+
+				System.out.println("계속할시 true, 그만할시 false");
+				more = Boolean.parseBoolean(scan.nextLine());
+				if (!more)
+					break EXIT;
+
+				break;
+
+			case 3:
+
+				break EXIT;
+
+			}
+		}
+		System.out.println("good bye~");
+
+	}
+
+}
